827a4551378bf58003a40e4c99701223c0425843
ksugawara61 <katsuo.com61@gmail.com>
2016-07-17 18:05:16 +0900

    12.2.5 エラー処理

--- app/src/main/java/org/example/locationtest/MainActivity.java
+++ app/src/main/java/org/example/locationtest/MainActivity.java
@@ -1,5 +1,8 @@
 package org.example.locationtest;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +10,8 @@ import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
@@ -120,4 +125,87 @@ public class MainActivity extends AppCompatActivity implements
             log(location.toString());
         }
     }
+
+    /**
+     * Google Playサービスが使えるかどうかをチェックする
+     */
+    private boolean serviceAvailable() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (ConnectionResult.SUCCESS == resultCode) {
+            log("Google Play services is available");
+            return true;
+        }
+        else {
+            log("Google Play services is not available");
+            showErrorDialog(resultCode);
+            return false;
+        }
+    }
+
+    /**
+     * Google Playサービスのエラーメッセージを表示する
+     */
+    private void showErrorDialog(int resultCode) {
+        // Google Playサービスからエラーダイアログを取得する
+        Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                resultCode, this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+        if (errorDialog != null) {
+            // エラーを表示する
+            errorDialog.show();
+        }
+    }
+
+    /**
+     * 位置情報クライアントへの接続が失敗した時に位置情報サービスから呼び出される
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // 例えば新バージョンをインストールするなどすれば解決するのか？
+        log("Connection failed");
+        if (connectionResult.hasResolution()) {
+            try {
+                // エラーの解決を試みるアクティビティを起動
+                log("Trying to resolve the error...");
+                connectionResult.startResolutionForResult(
+                        this, CONNECTION_FAILURE_RESOLUTION_REQUEST
+                );
+            } catch (IntentSender.SendIntentException e) {
+                log("Exception during resolution: " + e.toString());
+            }
+        }
+        else {
+            // 解決不能
+            showErrorDialog(connectionResult.getErrorCode());
+        }
+    }
+
+    /**
+     * クライアントサービスが一時的に接続切断状態になったときに位置情報サービスから呼び出される
+     */
+    @Override
+    public void onConnectionSuspended(int cause) {
+        log("Connection suspended");
+    }
+
+    /**
+     * Google Playサービスの新バージョンをダウンロードしたあとの処理をする
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case CONNECTION_FAILURE_RESOLUTION_REQUEST:
+                // 結果コードがOKなら、再接続を試みる
+                log("Resolution result code is: " + resultCode);
+                switch(resultCode) {
+                    case RESULT_OK:
+                        // ここで再接続を試す
+                        mGoogleApiClient.connect();
+                        break;
+                }
+                break;
+        }
+    }
+
 }
+
